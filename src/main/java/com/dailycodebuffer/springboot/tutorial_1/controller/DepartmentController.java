419e0d7a9383e386bbf860595b7cc175bfad06e2
MuaajSombare <sombaremuaaj6@gmail.com>
2024-07-19 15:45:19 +0530

    added new controller to fetch data

--- src/main/java/com/dailycodebuffer/springboot/tutorial_1/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/springboot/tutorial_1/controller/DepartmentController.java
@@ -3,9 +3,12 @@ package com.dailycodebuffer.springboot.tutorial_1.controller;
 import com.dailycodebuffer.springboot.tutorial_1.entity.Department;
 import com.dailycodebuffer.springboot.tutorial_1.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -15,5 +18,10 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartments(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartments(){
+        return departmentService.fetchDepartments();
     }
 }
