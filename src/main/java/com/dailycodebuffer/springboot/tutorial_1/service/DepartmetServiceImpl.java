c5302782cd66643ea1ad458459a6165d31c657d0
MuaajSombare <sombaremuaaj6@gmail.com>
2024-07-19 13:05:09 +0530

    added Entity, Controller layer, Service layer, Repository layer

---
+++ src/main/java/com/dailycodebuffer/springboot/tutorial_1/service/DepartmetServiceImpl.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.springboot.tutorial_1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmetServiceImpl implements DepartmentService{
+}
