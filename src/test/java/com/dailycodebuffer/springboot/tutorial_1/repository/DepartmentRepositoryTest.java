58ca1ea22189a4a47ef00550e947483460c43cb0
MuaajSombare <sombaremuaaj6@gmail.com>
2024-07-23 17:28:01 +0530

    Test case added for DepartmentRepository

--- src/test/java/com/dailycodebuffer/springboot/tutorial_1/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/springboot/tutorial_1/repository/DepartmentRepositoryTest.java
@@ -3,6 +3,7 @@ package com.dailycodebuffer.springboot.tutorial_1.repository;
 import com.dailycodebuffer.springboot.tutorial_1.entity.Department;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,18 +20,18 @@ class DepartmentRepositoryTest {
 
     @BeforeEach
     void setUp() {
+    }
 
-        Department department =
+    @Test
+    @Disabled
+    public void whenFindById_thenReturnDepartmen(){
+        Department department1 =
                 Department.builder()
                         .departmentName("Mechnical Engineering")
                         .departmentCode("ME-01")
                         .departmentAddress("Solapur")
                         .build();
-        entityManager.persist(department);
-    }
-
-    @Test
-    public void whenFindById_thenReturnDepartmen(){
+        entityManager.persist(department1);
         Department department = departmentRepository.findById(1L).get();
         assertEquals("Mechnical Engineering", department.getDepartmentName());
     }
