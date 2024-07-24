ab2abe05f3c119fe69078a71babe8c25883c882a
MuaajSombare <sombaremuaaj6@gmail.com>
2024-07-24 11:14:06 +0530

    added some test cases

--- src/test/java/com/dailycodebuffer/springboot/tutorial_1/service/DepartmentServiceTest.java
+++ src/test/java/com/dailycodebuffer/springboot/tutorial_1/service/DepartmentServiceTest.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.springboot.tutorial_1.service;
 
 import com.dailycodebuffer.springboot.tutorial_1.entity.Department;
+import com.dailycodebuffer.springboot.tutorial_1.error.DepartmentNotFoundException;
 import com.dailycodebuffer.springboot.tutorial_1.repository.DepartmentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -13,8 +14,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 class DepartmentServiceTest {
     @Autowired
@@ -32,14 +36,78 @@ class DepartmentServiceTest {
                         .departmentId(1L)
                         .build();
 
-        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+        when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
                 .thenReturn(Arrays.asList(department));
     }
     @Test
-    @Disabled
+//    @Disabled
     public void whenValidDepartmentName_thenDepartmentShouldFound(){
         String departmentName = "IT";
         List<Department> found = departmentService.fetchDepartmentByName(departmentName);
         assertEquals(departmentName, found.get(0).getDepartmentName());
     }
+    @Test
+    public void whenSaveDepartmen_thenShouldReturnDepartment(){
+        Department department1 =
+                Department.builder()
+                        .departmentName("IT")
+                        .departmentAddress("Ahmedabad")
+                        .departmentCode("ITC-001")
+                        .build();
+
+        Department department2 =
+                Department.builder()
+                        .departmentId(1l)
+                        .departmentName("IT")
+                        .departmentAddress("Ahmedabad")
+                        .departmentCode("ITC-001")
+                        .build();
+        when(departmentRepository.save(department1)).thenReturn(department2);
+        Department found = departmentService.saveDepartment(department1);
+
+        assertEquals(department2,found);
+    }
+
+    @Test
+    public void getAllDepartments(){
+        Department department1 =
+                Department.builder()
+                        .departmentName("IT")
+                        .departmentAddress("Ahmedabad")
+                        .departmentCode("ITC-001")
+                        .build();
+
+        when(departmentRepository.findAll()).thenReturn(Arrays.asList(department1));
+        List<Department> found = departmentService.fetchDepartments();
+        assertEquals(department1,found.get(0));
+    }
+
+    @Test
+    public void fetchDepartmentById() throws DepartmentNotFoundException {
+        Department department1 =
+                Department.builder()
+                        .departmentName("IT")
+                        .departmentAddress("Ahmedabad")
+                        .departmentCode("ITC-001")
+                        .build();
+
+        when(departmentRepository.findById(1l)).thenReturn(Optional.ofNullable(department1));
+        Department found = departmentService.fetchDepartmentById(1l);
+        assertEquals(department1,found);
+
+    }
+    @Test
+    public void fetchDepartmentById_throwsException_whenDepartmentNotFound() throws DepartmentNotFoundException {
+        when(departmentRepository.findById(1l)).thenReturn(Optional.empty());
+        try
+        {
+            departmentService.fetchDepartmentById(1l);
+        }
+        catch (DepartmentNotFoundException d){
+
+        }
+    }
+
+
+
 }
